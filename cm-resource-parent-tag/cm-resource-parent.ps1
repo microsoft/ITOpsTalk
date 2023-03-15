d0a0a84de2846ac468dbfb2c3f1743d9592ca8b5
Sonia Cuff <Sonia.Cuff@microsoft.com>
2023-03-15 12:23:51 +1000

    Initial cm-parent-resource tag script

---
+++ cm-resource-parent-tag/cm-resource-parent.ps1
@@ -0,0 +1,10 @@
+$ResourceParent = "vm-s19-01"
+$ParentID = Get-AzResource -Name $ResourceParent | Select-Object -ExpandProperty ResourceID
+$RG = "SVM-01-RG"
+$resourcelist = Get-AzResource -ResourceGroupName $RG 
+
+foreach($item in $resourcelist){
+    $tags = @{"cm-resource-parent"=$ParentID}
+    Update-AzTag -ResourceId $item.Resourced -Tag $tags -Operation Merge
+
+}
